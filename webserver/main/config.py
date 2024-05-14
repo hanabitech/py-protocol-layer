0b96e227c0a84764146279083b8f5392df70d323
HtDeepMakadiya <deep.makadiya@hanabitech.com>
2024-05-14 15:59:05 +0530

    refactor: removed not required TTL_IN_SECOND variable from config

--- webserver/main/config.py
+++ webserver/main/config.py
@@ -36,7 +36,6 @@ class Config:
     BAP_ID = os.getenv("BAP_ID", "buyer-app.ondc.org")
     BAP_UNIQUE_KEY_ID = os.getenv("BAP_UNIQUE_KEY_ID", "207")
     REGISTRY_BASE_URL = os.getenv("REGISTRY_BASE_URL", "https://staging.gateway.proteantech.in/ondc")
-    TTL_IN_SECONDS = int(os.getenv("TTL_IN_SECONDS", "18000"))
     VERIFICATION_ENABLE = os.getenv("VERIFICATION_ENABLE", "True") == "True"
     RABBITMQ_QUEUE_NAME = os.getenv("RABBITMQ_QUEUE_NAME", "bpp_protocol")
     RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "localhost")
