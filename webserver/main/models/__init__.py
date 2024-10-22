2e461881a9c7e916c47c2656097289304e0e6874
HtDeepMakadiya <deep.makadiya@hanabitech.com>
2024-10-22 19:02:41 +0530

    fix: revert changes

--- webserver/main/models/__init__.py
+++ webserver/main/models/__init__.py
@@ -40,10 +40,9 @@ def create_all_indexes():
     [create_ttl_index(c) for c in ["on_search_dump", "request_dump", "on_search_items",
                                                             "provider", "custom_menu", "location", "product",
                                                             "product_attribute", "product_attribute_value",
-                                                            "variant_group", "customisation_group"
+                                                            "variant_group", "customisation_group", "location_offer"
                                                             ]]
     get_mongo_collection("on_search_items").create_index([('id', TEXT)], name='id_index')
-    get_mongo_collection("location").create_index([("gps", GEOSPHERE)])
 
 
 def create_ttl_index(collection_name, ttl_in_seconds=None):
@@ -51,10 +50,8 @@ def create_ttl_index(collection_name, ttl_in_seconds=None):
     index_name = "created_at_ttl"
     # Set default TTL if not provided
     ttl_in_seconds = ttl_in_seconds if ttl_in_seconds else get_config_by_name('TTL_IN_SECONDS')
-
     # Get the current index information
     indexes = collection.index_information()
-
     # Check if the index exists and if the 'expireAfterSeconds' value matches
     if index_name in indexes:
         current_ttl = indexes[index_name].get('expireAfterSeconds')
@@ -69,6 +66,7 @@ def create_ttl_index(collection_name, ttl_in_seconds=None):
         # Create the index if it does not exist
         log(f"Creating index {index_name} on collection {collection_name} with TTL of {ttl_in_seconds} seconds.")
         collection.create_index([("created_at", ASCENDING)], name=index_name, expireAfterSeconds=ttl_in_seconds)
+
 
 def get_mongo_collection(collection_name):
     # check if database is initialized
